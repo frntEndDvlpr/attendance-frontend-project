8cdf8c8ee6ee8992f04a33bc15491c0d225a9876
frntEndDvlpr <reactdvlpr@gmail.com>
2025-01-01 12:31:24 +0400

    last

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-/* import "react-native-gesture-handler";
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 
 import AuthNavigator from "./app/navigation/AuthNavigator";
@@ -12,56 +12,9 @@ import SettingsScreen from "./app/screens/SettingsScreen";
 import SettingsNavigator from "./app/navigation/SettingsNavigator";
 
 export default function App() {
- return (
-   <NavigationContainer theme={NavigationTheme}>
-     <AuthNavigator />
-   </NavigationContainer>
- );
-} */
-
-import React, { useState, useEffect } from "react";
-import { Camera } from "expo-camera";
-import { View, Button, StyleSheet, Text } from "react-native";
-
-export default function OpenCamera() {
-  const [hasPermission, setHasPermission] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === "granted");
-    })();
-  }, []);
-
-  if (hasPermission === null) {
-    return <View />;
-  }
-  if (hasPermission === false) {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>No access to camera</Text>
-        <Button
-          title="Grant Permission"
-          onPress={Camera.requestCameraPermissionsAsync}
-        />
-      </View>
-    );
-  }
-
-  return <Camera style={styles.camera} />;
+  return (
+    <NavigationContainer theme={NavigationTheme}>
+      <AuthNavigator />
+    </NavigationContainer>
+  );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  camera: {
-    flex: 1,
-  },
-  text: {
-    fontSize: 18,
-    marginBottom: 10,
-  },
-});
