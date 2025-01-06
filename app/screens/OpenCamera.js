81ff96d7da2799f89b0bce594c91c32987c9d1d0
frntEndDvlpr <reactdvlpr@gmail.com>
2025-01-06 15:26:02 +0400

    Success

--- app/screens/OpenCamera.js
+++ app/screens/OpenCamera.js
@@ -1,93 +1,75 @@
-import {
-  Camera,
-  CameraView,
-  CameraType,
-  useCameraPermissions,
-} from "expo-camera";
-import { useRef, useState } from "react";
-import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import TakePhotoButton from "../components/TakePhotoButton";
-import AppIcon from "../components/AppIcon";
-import colors from "../config/colors";
+import React, { useState, useEffect } from "react";
+import { View, Button, Image, Alert, Text } from "react-native";
+import * as MediaLibrary from "expo-media-library";
+import { Camera, CameraView } from "expo-camera";
 
-export default function App() {
-  const [facing, setFacing] = useState("front");
-  const [permission, requestPermission] = useCameraPermissions();
-  const cameraRef = useRef(null);
-  const [Photo, setCPhoto] = useState(null);
+export default function OpenCamera() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [photo, setPhoto] = useState(null);
 
-  if (!permission) {
-    // Camera permissions are still loading.
-    return <View />;
-  }
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
 
-  if (!permission.granted) {
-    // Camera permissions are not granted yet.
-    return (
-      <View style={styles.container}>
-        <Text style={styles.message}>
-          We need your permission to show the camera
-        </Text>
-        <Button onPress={requestPermission} title="grant permission" />
-      </View>
-    );
-  }
-
-  function toggleCameraFacing() {
-    setFacing((current) => (current === "back" ? "front" : "back"));
-  }
+      const mediaLibraryPermission =
+        await MediaLibrary.requestPermissionsAsync();
+      if (mediaLibraryPermission.status !== "granted") {
+        Alert.alert("Permission to access media library is required!");
+      }
+    })();
+  }, []);
 
   const takePicture = async () => {
-    if (cameraRef.current) {
-      const options = { quality: 1, base64: true };
-      const photo = await takePictureAsync(options);
-      console.console.log("Photo URI", photo.uri);
-      setCPhoto(photo);
+    if (camera) {
+      const photoData = await camera.takePictureAsync();
+      setPhoto(photoData.uri);
+      savePhotoToCameraRoll(photoData.uri);
     }
   };
 
+  const savePhotoToCameraRoll = async (uri) => {
+    try {
+      await MediaLibrary.saveToLibraryAsync(uri);
+      Alert.alert(
+        "Photo saved!",
+        "Your photo has been saved to the camera roll."
+      );
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Error saving photo", "There was an error saving the photo.");
+    }
+  };
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <CameraView flash autofocus style={styles.camera} facing={facing}>
-        <View style={styles.cameraIcons}>
-          <TouchableOpacity style={styles.button}>
-            <AppIcon
-              name={"close-thick"}
-              size={60}
-              backgroundColor={colors.primaryTransparency}
-            />
-          </TouchableOpacity>
-          <TakePhotoButton onPress={takePicture} />
-          <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <AppIcon
-              name={"camera-flip-outline"}
-              size={60}
-              backgroundColor={colors.primaryTransparency}
-            />
-          </TouchableOpacity>
+    <View style={{ flex: 1 }}>
+      <CameraView style={{ flex: 1 }} ref={setCamera}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "flex-end",
+            alignItems: "center",
+            marginBottom: 20,
+          }}
+        >
+          <Button title="Take Picture" onPress={takePicture} />
         </View>
       </CameraView>
+
+      {photo && (
+        <Image
+          source={{ uri: photo }}
+          style={{ width: 100, height: 100, margin: 20 }}
+        />
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  message: {
-    textAlign: "center",
-    paddingBottom: 10,
-  },
-  camera: {
-    flex: 1,
-    justifyContent: "flex-end",
-  },
-  cameraIcons: {
-    height: "150",
-    //backgroundColor: "black",
-    justifyContent: "space-around",
-    flexDirection: "row",
-    alignItems: "center",
-  },
-});
