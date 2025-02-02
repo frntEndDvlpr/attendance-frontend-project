ec27cfd5bb7d73bc90c3ca6c6b9f221d29b5b7fc
frntEndDvlpr <reactdvlpr@gmail.com>
2025-02-02 08:46:26 +0400

    update URL

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,7 @@
 import { create } from "apisauce";
 
 const apiClient = create({
-  baseURL: "http://192.168.0.159:8000/api/",
+  baseURL: "http://192.168.100.65:8000/api/",
   timeout: 30000,
 });
 
