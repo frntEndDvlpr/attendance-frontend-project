7c740ae4e321ddf39399fe2040580a1c162d0265
frntEndDvlpr <reactdvlpr@gmail.com>
2025-03-10 14:18:36 +0400

    create and submit users

--- app/api/auth.js
+++ app/api/auth.js
@@ -1,6 +1,24 @@
 import apiClient from "./client";
 
-const getUsers = () => apiClient.get("auth/users/");
+const endPoint = "auth/users/";
+
+const getUsers = () => apiClient.get(endPoint);
+
+const adduser = (user, onUploadProgress) => {
+  const data = new FormData();
+  data.append("username", user.name);
+  data.append("email", user.email);
+  data.append("password", user.password);
+
+  console.log("Sending data to server:", data);
+
+  return apiClient.post(endPoint, data, {
+    onUploadProgress: (event) => {
+      const progress = event.loaded / event.total;
+      onUploadProgress(progress);
+    },
+  });
+};
 
 const login = (username, password) =>
   apiClient.post("token/", { username, password });
@@ -8,4 +26,4 @@ const login = (username, password) =>
 // Deleting a user from the server API
 const deleteUser = (id) => apiClient.delete(endPoint + id + "/");
 
-export default { login, getUsers, deleteUser };
+export default { login, adduser, getUsers, deleteUser };
