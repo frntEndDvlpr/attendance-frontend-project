48d2f665412ae8c8880df52c17ec5d77d71cbc94
Abbas <reactdvlpr@gmail.com>
2025-06-27 20:28:48 +0400

    ip update

--- app/config/settings.js
+++ app/config/settings.js
@@ -1,23 +1,23 @@
-import Constants from "expo-constants"
+import Constants from "expo-constants";
 
 const settings = {
-    dev: {
-        apiUrl: "http://192.168.100.101:8000/"
-    },
+  dev: {
+    apiUrl: "http://192.168.0.154:8000/",
+  },
 
-    staging: {
-        apiUrl: "http://192.168.0.117:8000/"
-    },
+  staging: {
+    apiUrl: "http://192.168.0.117:8000/",
+  },
 
-    prod: {
-        apiUrl: "http://192.168.0.117:8000/"
-    }
-}
+  prod: {
+    apiUrl: "http://192.168.0.117:8000/",
+  },
+};
 
 const getCurrentSettings = () => {
-    if (__DEV__) return settings.dev;
-    if (Constants.manifest.releaseChannel === "staging") return settings.staging;
-    return settings.prod
+  if (__DEV__) return settings.dev;
+  if (Constants.manifest.releaseChannel === "staging") return settings.staging;
+  return settings.prod;
 };
 
 export default getCurrentSettings();
